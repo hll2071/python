c93f64c36fd8f0361c4bb633fd56d291453c13f3
hsm <hll2071@naver.com>
2024-08-08 21:28:51 +0900

    백준 1076번

---
+++ b-1076.py
@@ -0,0 +1,5 @@
+color = ['black','brown','red','orange','yellow','green','blue','violet','grey','white']
+a = color.index(input()) * 10
+b = color.index(input())
+c = 10 ** color.index(input())
+print((a+b)*c)
